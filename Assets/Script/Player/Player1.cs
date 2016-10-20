fc510d090a91273ee5696632e44c11d3c93261a9
cydert <saida_service@outlook.jp>
2016-10-20 23:36:20 +0900

    add: playerMove sc

--- Assets/Script/Player/Player1.cs
+++ Assets/Script/Player/Player1.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class Player1 : MonoBehaviour {
-
+	public static int weight = 50;
 	// Use this for initialization
 	void Start () {
 	
---
+++ Assets/Script/Player/PlayerMove1.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove1 : MonoBehaviour {
+	private float speed = 10f;
+	private float rotation = 4f;
+	private string[] key1;
+	// Use this for initialization
+	void Start () {
+		key1 = new string[4];
+		key1[0] = "w";
+		key1[1] = "s";
+		key1[2] = "a";
+		key1[3] = "d";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(key1[0])) {	//前進
+			transform.position += transform.forward * speed * Time.deltaTime;
+		}
+		if (Input.GetKey (key1 [1])) {
+			transform.position += transform.forward * speed * Time.deltaTime * -1;
+		}
+
+		//回転
+		if (Input.GetKey (key1 [2])) {
+			transform.eulerAngles += new Vector3 (0f, rotation*-1, 0f);
+		}
+		if (Input.GetKey (key1 [3])) {
+			transform.eulerAngles += new Vector3 (0f, rotation, 0f);
+		}
+
+		//重力
+
+	}
+}
