4e9a7866db584777e2f36ec30b73c72184f13f20
cydert <saida_service@outlook.jp>
2016-10-27 19:40:20 +0900

    add: sc MoveWeapon.cs
    武器取得スクリプト追加

--- Assets/Script/Weapon/MoveWeapon.cs
+++ Assets/Script/Weapon/MoveWeapon.cs
@@ -2,7 +2,12 @@
 using System.Collections;
 
 public class MoveWeapon : MonoBehaviour {
-
+	private float angle = 45f;
+	private float locatonForward = 1f;
+	private Timer tmr;
+	private bool getCan = false;
+	private string viewTx;
+	private string getKey = "q";
 	// Use this for initialization
 	void Start () {
 	
@@ -11,5 +16,22 @@ public class MoveWeapon : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.CompareTag ("Player")) {	//Playerと触れたら
+			getCan = true;
+			viewTx = "Eキー";
+			if (Input.GetKey (getKey)) {
+				//武器入れ替え
+			}
+		}
+	}
+	void OnTriggerExit(Collider col){
+		getCan = false;
+	}
+
+	void OnGUI(){
+		if(getCan)
+			GUI.Label (new Rect (0, 0, 100, 30), viewTx);
 	}
 }
