39640a44aeb390dcf0f929049f33eaeda79fae11
cydert <saida_service@outlook.jp>
2016-10-27 18:50:43 +0900

    add: sc Timer
    Timerスクリプトの追加

---
+++ Assets/Script/Timer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+	private float start;
+	private float end;
+	Timer(float end){
+		start = Time.time;
+		this.end = end;
+	}
+	Timer(float first,float end){
+		this.start = first;
+		this.end = end;
+	}
+	float getTime(){
+		return Time.time - start;
+	}
+	bool getEndIs(){
+		if (Time.time - start >= end)
+			return true;
+		else
+			return false;
+	}
+	/*
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	*/
+}
