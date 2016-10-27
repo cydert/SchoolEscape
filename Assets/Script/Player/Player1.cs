dcc984b77afd4939e9380f2d16063666b4f1c570
cydert <saida_service@outlook.jp>
2016-10-28 04:39:34 +0900

    revert: add shovel

--- Assets/Script/Player/Player1.cs
+++ Assets/Script/Player/Player1.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 public class Player1 : MonoBehaviour {
-	public static GameObject weapon = null;
+	public static GameObject noActiveWeapon = null;
+	public static bool weaponB = false;
+	public static GameObject playerObj;
 	// Use this for initialization
 	void Start () {
-		
+		playerObj = this.gameObject;
 	}
 	
 	// Update is called once per frame
--- Assets/Script/Weapon/MoveWeapon.cs
+++ Assets/Script/Weapon/MoveWeapon.cs
@@ -5,47 +5,51 @@ public class MoveWeapon : MonoBehaviour {
 	private float angle = 45f;
 	private float locatonForward = 1f;
 	private Timer tmr;//時間差スクリプト有効用
-	private bool fin;//最終スクリプト処理をするか
+	private bool fin = false;//最終スクリプト処理をするか
 	private GameObject oldObj;
 	private bool getCan = false;//表示用
 	private string viewTx;
 	private string getKey = "q";
-	public GameObject pObj;
+	private GameObject pObj;
+
+	public float upY;//各武器誤差対処用 交換時
 	// Use this for initialization
 	void Start () {
-	
+		pObj = Player1.playerObj;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (getCan) {
-			if (Input.GetKey (getKey)) {
-				getCan = false;
-				//武器入れ替え
-				//所持武器を捨てる
-				if (Player1.weapon != null) {
-					transform.Rotate(new Vector3(0f,0f,0f));
-					Player1.weapon.transform.parent = null;//子解除
-					fin = true;
-					oldObj = Player1.weapon;
-				}
-				//武器装備
-				Player1.weapon = gameObject;
-				transform.parent = pObj.transform;//子として登録
-				transform.position = pObj.transform.forward + pObj.transform.forward * locatonForward;//装備場所指定
-				transform.Rotate(new Vector3(0f,0f,45f));
-
-				getCan = false;
-				tmr = new Timer (0.2f);
-			}
-		}
-
 		//終了処理
 		if (fin && tmr.getEndIs()) {
 			fin = false;
 			oldObj.GetComponent<MoveWeapon> ().enabled = true;//置いたオブジェクトのスクリプト有効化
 			this.GetComponent<MoveWeapon>().enabled = false;//このスクリプト無効
+		}else if (getCan) {
+			if (Input.GetKey (getKey)) {
+				getCan = false;
+				//武器入れ替え
+				//所持武器を捨てる
+				if (Player1.noActiveWeapon != null) {
+					Player1.noActiveWeapon.transform.parent = null;//子解除
+					Player1.noActiveWeapon.SetActive(true);//再表示
+
+					fin = true;
+					oldObj = Player1.noActiveWeapon;
+				}
+				//武器装備
+				gameObject.transform.parent = pObj.transform;//子として登録
+				Player1.noActiveWeapon = gameObject;
+				gameObject.transform.position = Player1.playerObj.transform.position + new Vector3(0f,upY,0f);
+
+
+				gameObject.SetActive(false);//非表示
+				getCan = false;
+				tmr = new Timer (0.2f);
+			}
 		}
+
+
 	}
 
 	void OnTriggerEnter(Collider col){
