b12434291686c2f835a39050843861da18e25e56
cydert <saida_service@outlook.jp>
2016-10-29 06:50:20 +0900

    fix: weapon script

---
+++ Assets/Script/Monstar/Des.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Des : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter(Collision col){
+		Debug.Log (col.gameObject.tag);
+		if (col.gameObject.CompareTag("weapon")) {
+			//Destroy(this.gameObject);
+			GameObject.Destroy (gameObject);
+		}
+	}
+	/*
+	void OnTriggerEnter(Collider col){
+		Debug.Log ("ss1");
+		Debug.Log (col.gameObject.tag);
+		if (col.gameObject.CompareTag("weapon")) {
+			Debug.Log ("s1");
+			//Destroy(this.gameObject);
+			GameObject.Destroy (gameObject);
+		}
+	}
+	*/
+}
---
+++ Assets/Script/Monstar/MonstarMove.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonstarMove : MonoBehaviour {
+	public Transform player;
+	public float speed = 4f;
+	private bool rockOn;
+	public bool save = true;//生存してるか
+	// Use this for initialization
+	void Start () {
+		player  = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (rockOn && save) {
+			Vector3 direc = player.position - transform.position;
+			direc = direc.normalized;
+			transform.position += direc * speed * Time.deltaTime;
+			transform.LookAt (player);
+		}
+	}
+	void OnTriggerEnter(Collider col){
+		if (col.tag == "Player") {
+			rockOn = true;
+		} else if (col.tag == "weapon") {
+			Destroy (gameObject);
+		}
+	}
+	void OnTriggerExit(Collider col){
+		if (col.tag == "Player") {
+			rockOn = false;
+		}
+	}
+}
